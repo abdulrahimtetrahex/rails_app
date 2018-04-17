7149a0f026102ba83b7f8b56b569975b0e613f05
abdul-rahim <abdulrahim@tetrahex.com>
2018-04-17 10:35:16 +0500

    Images added to post using paperclip gem

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'devise'
 # User CanCanCan
 gem 'cancancan', '~> 2.0'
-
+# paperclip
+gem "paperclip", "~> 6.0.0"
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416041614) do
+ActiveRecord::Schema.define(version: 20180417052429) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20180416041614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
